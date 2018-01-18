8961b1b45ec0448095388b09db7a719437d4ec93
yalonsob <yalonsob@gmail.com>
2018-01-18 08:00:07 -0800

    Basic version of the TipApp

---
+++ TipApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  TipApp
+//
+//  Created by Yolanda Alonso on 1/18/18.
+//  Copyright © 2018 Yolanda Alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages =  [0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        tipLabel.text = String (format: "$%.2f", tip)
+        totalLabel.text = String (format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+}
+
